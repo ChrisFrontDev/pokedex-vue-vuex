c3e324f931dc9b2348f383963c32a94dc6ba94c0
Christian Santosm <christian.santos@devires.com>
2021-01-13 16:01:14 -0300

    initial config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    "browser": true,
+    "es6": true,
+    "node": true
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
@@ -9,6 +11,11 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'global-require': 'warn',
+    'no-prototype-builtins': 'warn',
+    'no-shadow': 'warn',
+    'no-cond-assign': ['warn', 'always'],
+    'no-undef': ['error', { typeof: true }],
   }
 };
